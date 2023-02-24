74775743474b6b4ee2b86a7c32998426d7bcbb96
Yehya22 <yehya.alsager@gmail.com>
2023-02-24 10:17:26 +0200

    Add Comments Section

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,23 +1,18 @@
-import MusicNoteIcon from "@mui/icons-material/MusicNote";
-import HomeIcon from "@mui/icons-material/Home";
 import CodeIcon from "@mui/icons-material/Code";
-import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
 import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
 import LiveTvIcon from "@mui/icons-material/LiveTv";
 import SchoolIcon from "@mui/icons-material/School";
-import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
-import CheckroomIcon from "@mui/icons-material/Checkroom";
 import GraphicEqIcon from "@mui/icons-material/GraphicEq";
-import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+import ForestIcon from "@mui/icons-material/Forest";
+import TheatersIcon from "@mui/icons-material/Theaters";
 
 export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
 export const categories = [
-  { name: "New", icon: <HomeIcon /> },
   { name: " Javascript", icon: <CodeIcon /> },
-  { name: "Coding", icon: <CodeIcon /> },
   { name: "ReactJS", icon: <CodeIcon /> },
   { name: "NextJS", icon: <CodeIcon /> },
   { name: "Education", icon: <SchoolIcon /> },
@@ -25,8 +20,10 @@ export const categories = [
   { name: "Gaming", icon: <SportsEsportsIcon /> },
   { name: "Live", icon: <LiveTvIcon /> },
   { name: "Sport", icon: <FitnessCenterIcon /> },
-  { name: "Comedy", icon: <TheaterComedyIcon /> },
+  { name: "News", icon: <NewspaperIcon /> },
   { name: "Gym", icon: <FitnessCenterIcon /> },
+  { name: "Documentary ", icon: <TheatersIcon /> },
+  { name: "Nature", icon: <ForestIcon /> },
   { name: "Crypto", icon: <DeveloperModeIcon /> },
 ];
 
